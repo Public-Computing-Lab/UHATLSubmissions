2331a73465ab3dc12c7c18028d4984920f82741e
Abhinav Vinod <avinod34@gatech.edu>
2025-07-25 23:07:29 -0400

    Eslint change

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,35 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // TypeScript - more permissive but still catch real issues
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": ["warn", { 
+        "argsIgnorePattern": "^_",
+        "varsIgnorePattern": "^_"
+      }],
+      "@typescript-eslint/no-non-null-assertion": "off",
+      "@typescript-eslint/ban-ts-comment": ["warn", {
+        "ts-ignore": "allow-with-description",
+        "ts-expect-error": "allow-with-description"
+      }],
+      
+      // React - allow common patterns
+      "react/no-unescaped-entities": "off",
+      "react-hooks/exhaustive-deps": "warn",
+      
+      // Next.js - more flexible
+      "@next/next/no-img-element": "warn",
+      
+      // General - still catch bugs but be reasonable
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "prefer-const": "warn",
+      "no-debugger": "error",
+      "no-unreachable": "error",
+      "no-undef": "error",
+    }
+  }
 ];
 
 export default eslintConfig;
